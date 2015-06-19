d3db82ac897ea16b6136c2f7fcc5838b26b91f73
treyyoder <jyoder@DIT-JT-YODER.eventcentric.local>
2015-06-19 17:06:21 -0400

    Revising map layout
    Moving the recall selector to a map control.
    Adding stubs for brand and details.

--- client/templates/map.js
+++ client/templates/map.js
@@ -6,7 +6,7 @@ LUtil = {
   // init function to be called ONCE on startup
   initLeaflet: function(){    
     $(window).resize(function() {
-      $('#map').css('height', window.innerHeight - 82 - 45);
+      $('#map').css('height', window.innerHeight);
     });
     $(window).resize(); // trigger resize event
   },
@@ -17,17 +17,20 @@ LUtil = {
     // sensible defaults if nothing specified
     element = element || 'map';
     view = view || {};
-    view.zoom = view.zoom || 4;
-    view.latlong = view.latlong || [ 37.8, -96 ];
-    var baseLayer = L.tileLayer('https://{s}.tiles.mapbox.com/v3/cwhong.map-hziyh867/{z}/{x}/{y}.png',{
+    view.zoom = view.zoom || 5;
+    view.latlong = view.latlong || [37.8, -92];
+    var baseLayer = L.tileLayer('https://{s}.tiles.mapbox.com/v3/{id}/{z}/{x}/{y}.png',{
       attribution: "<a href='https://www.mapbox.com/about/maps/' target='_blank'>&copy; Mapbox &copy; OpenStreetMap</a> <a class='mapbox-improve-map' href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a>",
-      maxZoom: 18
-    });  
+      maxZoom: 18,id: 'examples.map-20v6611k'
+    });  	
+	
     this.map = L.map(element, { 
         scrollWheelZoom : false,
         doubleClickZoom : false,
+		dragging		: false,
         boxZoom         : false,
         touchZoom       : false,
+		zoomControl		: false,
         layers: [baseLayer]
     })
     .setView(
@@ -36,13 +39,38 @@ LUtil = {
     );
     
     Tracker.autorun(function() {
-      if (!Session.get('LatestFoodRecallsIsReady')) return;
       var latestFoodRecallsCursor = FoodRecalls.find({});        
       self.latestFoodRecalls = latestFoodRecallsCursor.fetch();
       if (Meteor.settings.debug) console.log('latestFoodRecalls: ', self.latestFoodRecalls);
+      
       // TODO: do something with the data
+      
     });
+	
+	/*geojson = L.geoJson(statesData, {
+		style:styleDefault,
+		onEachFeature: onEachFeature
+	}).addTo(map);*/
+    
+    this.addControls();
   },
+  styleDefault: function(feature) {
+		return {
+			weight: 2,
+			opacity: 1,
+			color: 'black',
+			dashArray: '3',
+			fillOpacity: 0.7,
+			fillColor: 'white'
+		};
+	},
+	onEachFeature: function (feature, layer) {
+			layer.on({
+				mouseover: highlightFeature,
+				mouseout: unHighlightFeature,
+				click: zoomToFeature
+			});
+		},
   // register event handlers
   handleEvent: function(event, callback){
     if (!event || !callback) 
@@ -69,11 +97,42 @@ LUtil = {
     _obj = _obj || "";
     L.tileLayer( _layer, _obj)
       .addTo(this.map);
+  },
+  addControls: function(){
+	var recallSelector = L.control({position: 'topright'});
+	recallSelector.onAdd = function (map) {
+		var div = L.DomUtil.create('div', 'info legend');
+		div.innerHTML = '<b>Select a Recall:</b> <br> <div id="recallSelector"></div>';
+		return div;
+	};
+	recallSelector.addTo(this.map);	
+	
+	$("#latestFoodRecalls").appendTo("#recallSelector");  	
+	$("#latestFoodRecalls").select2();
+	
+	var details = L.control({position: 'bottomright'});
+	details.onAdd = function (map) {
+		var div = L.DomUtil.create('div', 'info legend');
+		div.innerHTML = '<b>Details</b>';
+		return div;
+	};
+	details.addTo(this.map);
+	
+	
+	var logo = L.control({position: 'bottomleft'});
+	logo.onAdd = function (map) {
+		var div = L.DomUtil.create('div', 'info legend');
+		div.innerHTML = '<b>FoodWatch</b>';
+		return div;
+	};
+	logo.addTo(this.map);	
+	
   }
 };
 Template.map.events({
   'change select' : function(event, template){    
     var val = $(event.currentTarget).val();
+	alert(val);
     if (Meteor.settings.debug) console.log('change select val:', val);
   }
 });
@@ -85,12 +144,6 @@ Template.map.helpers({
 Template.map.created = function(){};
 Template.map.rendered = function(){
   // Initialize the map view
-  LUtil.initMap();
-  // Add any additional layers 
-  /*LUtil.addTileLayer('http://a{s}.acetate.geoiq.com/tiles/acetate-labels/{z}/{x}/{y}.png', {
-    attribution: '&copy;2012 Esri & Stamen, Data from OSM and Natural Earth',
-    subdomains: '0123',
-    minZoom: 2,
-    maxZoom: 18
-  });*/
+  LUtil.initMap();  
+  
 };
